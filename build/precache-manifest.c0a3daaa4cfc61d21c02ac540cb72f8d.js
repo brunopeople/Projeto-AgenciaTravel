399ee50718c8f1a93403c1038523118e6e2b2503
bruno pessoa <brunopessoa.melo@gmail.com>
2020-09-21 23:56:03 -0300

    subindo o projeto no repositório, testando a aplicação, fazendo o build do projeto

---
+++ build/precache-manifest.c0a3daaa4cfc61d21c02ac540cb72f8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3263d4d9cb41a95c6d125cfb23781b19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f18b9920fc94ad7a809a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d1eb55963fed2465c99",
+    "url": "/static/js/2.7054c213.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7054c213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18b9920fc94ad7a809a",
+    "url": "/static/js/main.6bbfb45d.chunk.js"
+  },
+  {
+    "revision": "a6cea561a05f2a3dd963",
+    "url": "/static/js/runtime-main.0939eb42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
