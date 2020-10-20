124510a5961c33c53531d5ff182f986f195139b3
bruno pessoa <brunopessoa.melo@gmail.com>
2020-10-20 15:09:41 -0300

    erro de compilação dos componentes, adicionando o código do App.css.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {BrowserRouter as Router} from 'react'
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router><App /></Router>, 
+    document.getElementById('root'));
 
